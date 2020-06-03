40d68dce5bdb0e31b255e7e7e9515d6ae2e6a285
sausagemountain <miha.vanya@gmail.com>
2020-06-04 01:48:11 +0500

    Update drag-target

--- app/components/drag-target.js
+++ app/components/drag-target.js
@@ -9,35 +9,18 @@ export default class DragTargetComponent extends Component {
     super(...arguments);
     if(this.args.elementsCollection)
       this.elementsCollection = this.args.elementsCollection
-
-    if (this.args.Name)
-      this.elementsCookieName = this.args.Name + 'Cookie'
-
-    let list = App.getCookie(this.elementsCookieName)
-    if (list)
-      this.elementsCollection.pushObjects(list)
-
-    this.elementsCollection.addArrayObserver(this, {
-      willChange: 'elementsWillChange',
-      didChange: 'elementsDidChange'
-    })
   }
-
-  elementsWillChange() {
-    //console.log(arguments)
-  }
-
-  elementsDidChange() {
-    App.setCookie(this.elementsCookieName, this.elementsCollection)
-  }
-
-  elementsCookieName = 'graphItemsCookie'
   @tracked
   elementsCollection = A([])
 
   @action
   clear() {
     this.elementsCollection.clear()
+  }
+
+  @action
+  addItem() {
+    this.args.addItem()
   }
 
   @action

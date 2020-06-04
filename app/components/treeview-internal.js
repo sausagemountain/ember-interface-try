193f77c2b210f1249e9e49809fe942a5baee44e4
sausagemountain <miha.vanya@gmail.com>
2020-06-04 22:59:49 +0500

    Add editButton to treeview

--- app/components/treeview-internal.js
+++ app/components/treeview-internal.js
@@ -1,24 +1,35 @@
 import Component from '@glimmer/component';
 import {tracked} from "@glimmer/tracking";
 import {A} from '@ember/array'
+import { action } from '@ember/object';
 
 export default class TreeviewInternalComponent extends Component {
   constructor() {
     super(...arguments);
     if (this.args.node.items)
       this.items = A(this.args.node.items)
+
     if (this.args.node.canAdd !== undefined)
-      this.canAdd = A(this.args.node.canAdd)
+      this.canAdd = this.args.node.canAdd
+
+    if (this.args.node.canEdit !== undefined)
+      this.canEdit = this.args.node.canEdit
+
     if (this.args.node.canRemove !== undefined)
-      this.canRemove = A(this.args.node.canRemove)
+      this.canRemove = this.args.node.canRemove
+
     if(this.args.isDisplayed !== undefined)
       this.isDisplayed = this.args.isDisplayed
+
     if(this.args.node.isDisplayed !== undefined)
       this.isDisplayed = this.args.node.isDisplayed
   }
 
   @tracked
   canAdd = true
+
+  @tracked
+  canEdit = false
 
   @tracked
   canRemove = true
@@ -36,6 +47,19 @@ export default class TreeviewInternalComponent extends Component {
     }
   }
 
+  get currentItem(){
+    return {
+      data:{
+        name: this.args.node.node,
+        path: this.args.parent.path,
+        data: this.args.node.data,
+        index: this.args.index,
+      },
+      index: this.args.index,
+      list: this.args.parent.items,
+    }
+  }
+
   get path() {
     if (this.args.parent)
       {
@@ -50,7 +74,8 @@ export default class TreeviewInternalComponent extends Component {
     return ''
   }
 
-  toggleDisplay = () => {
+  @action
+  toggleDisplay() {
     this.isDisplayed = !this.isDisplayed
   }
 }

00ea79e159a9d0a54959c7e05f0cb0a8df487e0a
sausagemountain <miha.vanya@gmail.com>
2020-06-04 22:59:14 +0500

    Make edit-property-button work

--- app/components/edit-property-button.js
+++ app/components/edit-property-button.js
@@ -1,4 +1,12 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class EditPropertyButtonComponent extends Component {
+  @action
+  async editItem() {
+    const editData = this.args.value
+    const result = await this.args.action(editData.data)
+    editData.list.removeAt(editData.index)
+    editData.list.insertAt(result.index, result.data)
+  }
 }

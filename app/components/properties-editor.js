5c9f8de5fdf637c723d6f72c29b9dcddb98cea97
sausagemountain <miha.vanya@gmail.com>
2020-06-07 01:49:29 +0500

    Add range type to properties-editor

--- app/components/properties-editor.js
+++ app/components/properties-editor.js
@@ -1,51 +1,61 @@
 import Component from '@glimmer/component';
 import {readData} from "../excel-transformer";
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
 
 export default class PropertiesEditorComponent extends Component {
   constructor() {
     super(...arguments);
-    for(let key in this.args.properties){
+    for (let key in this.args.properties){
       const val = this.args.properties[key]
       let a = {
         val: val
       }
-      switch (typeof val) {
-        case "bigint":
-        case "number": {
-          a.regex = '(-)?\\d+(\\.\\d+)?'
-          a.type = 'number'
-          break
-        }
+      if (this.args.value[key] !== undefined && this.args.value[key] !== null){
+        a.val = this.args.value[key]
+      }
+      if (typeof val === "boolean") {
+        {
+          a.checkbox = a.type = 'checkbox'
 
-        case "boolean":{
-          a.type = 'checkbox'
-          a.checkbox = true
-          break
         }
+      } else if (typeof val === "undefined" || typeof val === "symbol" || typeof val === "string") {
+        {
+          if (val === '#') {
+            a.type = 'color'
+          } else {
+            a.type = 'text'
+          }
 
-        case "undefined":
-        case "symbol":
-        case "string":{
-          a.regex = '.*'
-          a.type = 'text'
-          break
         }
-        case "object":{
+      } else if (typeof val === "bigint" || typeof val === "number" || val instanceof Number) {
+        {
+          if (val.min !== undefined && val.max !== undefined) {
+            a.range = a.type = 'range'
+            a.min = val.min
+            a.max = val.max
+          } else {
+            a.regex = '(-)?\\d+(\\.\\d+)?'
+            a.type = 'number'
+          }
+
+        }
+      } else if (typeof val === "object") {
+        {
           if (Array.isArray(val)) {
-            if(val.length >= 0){
-              if (Array.isArray(val[0])){
+            if (val.length >= 0) {
+              if (Array.isArray(val[0])) {
                 a.data = val
-              }
-              else {
+              } else {
                 a.options = val;
               }
             }
+          } else {
+            a.object = val
+            if (this.args.value[key] === undefined || this.args.value[key] === null) {
+              this.args.value[key] = {}
+            }
           }
-          else {
-            a.object = true;
-          }
-          break
+
         }
       }
       this.props[key] = a
@@ -64,6 +74,7 @@ export default class PropertiesEditorComponent extends Component {
     const files = event.target.files;
     const reader = new FileReader()
     reader.readAsArrayBuffer(files[0])
+    const name = files[0].name
     reader.onloadend = () => {
       const data = readData(reader.result)
       for (let key1 in data) {
